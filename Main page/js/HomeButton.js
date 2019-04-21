3401bafc1f354a6084b8ffdc9be11f7dec0e3d80
Mikhail Sokolov <39666611+msokolov93@users.noreply.github.com>
2019-04-21 15:43:20 +0300

    Home Button fixed
    Code fixed for home button on every page.

--- Main page/js/HomeButton.js
+++ Main page/js/HomeButton.js
@@ -1,18 +1,31 @@
 
-	var homeVisible = 0;
-	document.getElementById("home0").onmouseover = function(){		
-		homeVisible = 1;		
-	}
+	var homeVisible = 0;	
+	var home0List = document.getElementsByClassName("btnHomeMain");
+	var homeButtonList = document.getElementsByClassName("HomeButton");
+	var btnHomeList = document.getElementsByClassName("btnHome");
 	
-	document.getElementById("homeButton").onmouseover = function(){		
-		if (homeVisible == 1){
-			document.getElementById("home2").style.visibility = "visible";
-			document.getElementById("home1").style.visibility = "visible";
+	for(var i=0; i < home0List.length; i++) { 
+		home0List[i].onmouseover = function(){
+			homeVisible = 1;
 		}
 	}
 	
-	document.getElementById("homeButton").onmouseleave = function(){
-		homeVisible = 0;
-		document.getElementById("home2").style.visibility = "hidden";
-		document.getElementById("home1").style.visibility = "hidden";
-	}
+	for(var i=0; i < homeButtonList.length; i++){
+		
+		homeButtonList[i].onmouseover = function(){
+			//console.log("OnMouseOver worked!");
+			if (homeVisible == 1){
+				for(var j=0; j < btnHomeList.length; j++){
+					btnHomeList[j].style.visibility = "visible";
+				}
+			}
+		}
+		
+		homeButtonList[i].onmouseleave = function(){
+			//console.log("OnMouseLeave worked!");
+			homeVisible = 0;
+			for(var j=0; j < btnHomeList.length; j++){
+				btnHomeList[j].style.visibility = "hidden";
+			}
+		}
+	}	
--- Main page/js/personScript.js
+++ Main page/js/personScript.js
@@ -1,14 +1,15 @@
 
 
-function startPersonScene(){
-	var centerY = (window.innerHeight - 630) / 2;
-	
-	document.getElementById("personcontainer").style.marginTop = centerY + "px";
+function startPersonScene(){	
+	//var centerY = (window.innerHeight - 630) / 2;
+	//document.getElementById("personcontainer").style.marginTop = centerY + "px";
+	document.getElementById("canvascontainer").style.display = "none";
 	document.getElementById("personcontainer").style.display = "block";
+	animatePersonScene();
 }
 	//var clientX, clientY;
-	
-	/*
+
+function animatePersonScene(){
 	document.onmousemove = function (e) {
 		var clientX = e.clientX;
 		var clientY = e.clientY;
@@ -16,10 +17,11 @@ function startPersonScene(){
 		var sideX = ( (window.innerWidth / 2) - clientX) / (window.innerWidth /2);
 		var sideY = ( (window.innerHeight / 2) - clientY) / (window.innerWidth /2);
 		
-		console.log(sideX);
+		//console.log(sideX);
 		document.getElementById("me").style.marginLeft = sideX*20 + 'px';
 		document.getElementById("me").style.marginTop = sideY*10 + 'px';
 		
 		document.getElementById("myhand").style.marginLeft = sideX*10 + 'px';
 		document.getElementById("myhand").style.marginTop = sideY*10 + 'px';
-	}	*/
+	}
+}
